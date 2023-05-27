d559eeb6630ef5a49f97975fc176c72b017cf185
Habibul Islam <habibulislam6862@gmail.com>
2023-05-27 13:59:09 +0600

    get specific chef details with id-route added

--- routes/chef.js
+++ routes/chef.js
@@ -2,6 +2,9 @@ const route = require("express").Router();
 const fs = require("fs/promises");
 const path = require("path");
 
+/**
+ * Get all chefs 
+ */
 route.get("/", async (req, res) => {
   try {
     let chefList = await fs.readFile(
@@ -14,6 +17,44 @@ route.get("/", async (req, res) => {
   }
 });
 
+/**
+ * get specific chef details  
+ */
+route.get("/:id", async (req, res) => {
+    let { id } = req.params;
+    id = Number(id);
+    try {
+      let chefList = await fs.readFile(
+        path.join(path.dirname(require.main.filename), "fakeDB", "chef.json")
+      );
+      chefList = JSON.parse(chefList);
+  
+      // binary search
+      let low = chefList[0]._id;
+      let high = chefList[chefList.length - 1]._id;
+      let mid = parseInt((high + low) / 2);
+      for (;;) {
+        if (id === mid) {
+          console.log(chefList[id - 1]);
+          break;
+        } else if (id > mid) {
+          low = mid + 1;
+          mid = parseInt((high + low) / 2);
+        } else if (id < mid) {
+          high = mid - 1;
+          mid = parseInt((high + low) / 2);
+        }
+  
+        // Not found condition
+        if (low > high) {
+          res.status(404).json({ message: "Chef Not Found" });
+          break;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
 
 module.exports = route;
