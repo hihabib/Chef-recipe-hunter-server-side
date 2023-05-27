c7a720d5e20a09ebf065225ccb62dce90926053a
Habibul Islam <habibulislam6862@gmail.com>
2023-05-27 19:45:32 +0600

    recipe controller added

---
+++ controllers/recipe.js
@@ -0,0 +1,10 @@
+const service = require("../services/recipe.js");
+
+const getRecipesByChefIdController = async (req, res) => {
+  let { id } = req.params;
+  id = Number(id);
+  const getRecipes = await service.getRecipesByChefIdService(id);
+  res.status(200).json(getRecipes);
+};
+
+module.exports = { getRecipesByChefIdController };
